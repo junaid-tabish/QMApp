172d156bc9ed9891c44726f00395601d48c3b217
Junaid Abdul <junaid.abdul@neosoftmail.com>
2023-04-04 09:27:31 +0530

    QAM

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { signup, login } = require("../controllers/UserController");
+//user routes
 
 router.post("/signup", signup);
 router.post("/login", login);
